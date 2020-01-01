d7e894f68a6c33fedc45bd495114925e45f5483d
Ryan Ritchie <ryan.dav.ritchie@gmail.com>
2020-01-01 22:14:23 +0000

    Added threading to directory scan

--- wavs.py
+++ wavs.py
@@ -10,6 +10,7 @@ import sys
 import requests
 import argparse
 import unittest
+from multiprocessing import Pool
 
 # my imports
 from utils import success, warning, info
@@ -27,9 +28,11 @@ class WebScanner():
         self.host = host
         self.port = port
 
-    def scan(self, wordlist_path):
-        print('[*] Starting scan on {}:{}', self.host, self.port)
+        # TODO: add way to change success codes
+        self.success_codes = [200, 201, 202, 203, 204, 301, 302, 303, 304]
+        self.directories_found = []
 
+    def _check_wrong_200(self):
         #TODO: need to try random long string to see if we get 200
         # that means the application is returning 200 for everything
         # need to fuzz
@@ -39,16 +42,33 @@ class WebScanner():
         if resp.status_code == 200:
             warning('/{} returned code 200. Should swith to fuzzing'.format(should_not_find))
 
-        for word in open(wordlist_path, 'r').readlines():
-            word = word.strip().strip('\n')
+    def _thread_scan(self, word):
+        resp = requests.get('http://{}:{}/{}'.format(self.host, self.port, word))
 
-            if word and word[0] == '#':
-                continue
-            resp = requests.get('http://{}:{}/{}'.format(self.host, self.port, word))
+        if (resp.status_code in self.success_codes):
+            return word
 
-            # TODO: handle different success response code
-            if (resp.status_code in [200, 302]):
-                print('/{}'.format(word))
+    def scan(self, wordlist_path):
+        print('[*] Starting scan on {}:{}'.format(self.host, self.port))
+
+        # check if application returns 200 for random string
+        self._check_wrong_200()
+
+        thread_pool = Pool(8)
+        # read lines in from the wordlist
+        # TODO: store word lists in database
+        with open(wordlist_path, 'r') as f:
+            word_list = f.read()
+            word_list = word_list.split('\n')
+
+        self.directories_found = thread_pool.map(self._thread_scan, word_list)
+        self.directories_found = [directory for directory in self.directories_found if directory]
+
+        thread_pool.close()
+        thread_pool.join()
+
+        print(self.directories_found)
+
 
 class Test(unittest.TestCase):
     pass
